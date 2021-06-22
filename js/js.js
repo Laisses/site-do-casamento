50f211b25897d1ac428cce920ddd8bc8d634be12
Laisses <71999249+Laisses@users.noreply.github.com>
2021-06-22 01:39:55 -0300

    js.js: Resolve problema de responsividade do menu hamburguer com MediaQueryList.

--- js/js.js
+++ js/js.js
@@ -10,5 +10,21 @@ function trocaBotoesDoMenu() {
 	}
 }
 
+//Tem que deixar a variável fora da função porque ele que informa qual o media query??? 
+//You can create a MediaQueryList by calling matchMedia() on the window object
+//The Window interface's matchMedia() method returns a new MediaQueryList object that can then be used to determine if the document matches the media query string, as well as to monitor the document to detect when it matches (or stops matching) that media query.
+
+let viewport = window.matchMedia("(min-width: 1024px)");
+
+
+function lidaComMenuHamburguerQuandoATelaTaGrandeMomoMeAjudaNaoEntendiDireitoNemSeiNomearFuncoes() {
+	if (viewport.matches) {
+	document.querySelector(".cabecalho__menu--hamburguer").style.display = "none";
+	}	
+}
+
+viewport.onchange = lidaComMenuHamburguerQuandoATelaTaGrandeMomoMeAjudaNaoEntendiDireitoNemSeiNomearFuncoes;
+
+
 document.querySelector(".botao__icone--hamburguer").onclick = trocaBotoesDoMenu;
 document.querySelector(".botao__icone--fecha").onclick = trocaBotoesDoMenu;
